6736a89ca13c331791e30c17447c8db669bb7119
Thurston Cox <thurstoncox3@gmail.com>
2021-09-14 21:47:25 -0400

    set my test

--- __Test__/Engineer.test.js
+++ __Test__/Engineer.test.js
@@ -1,1 +1,20 @@
-const engineer = require('../lib/engineer');
+const Engineer = require('../lib/engineer');
+const { it } = require('@jest/globals');
+
+it ('office number should be presented', ()=> {
+  const value = 'Software'
+  const e = new Engineer('Thurston', 896, 'thurstcx@yahoo.com', value);
+  expect(e.gitHub).toBe(value);
+});
+
+test ('getgitHub() should return officeNumebr', ()=> {
+  const value = 'gitHub'
+  const e = new Engineer('Thurston', 896, 'thurstcx@yahoo.com', value);
+  expect(e.getgitHub).toBe(value);
+});
+
+test ('gotRole() should return \Engineer\'', ()=> {
+  const value = 'Software'
+  const e = new Engineer('Thurston', 896, 'thurstcx@yahoo.com', value);
+  expect(e.getRole).toBe(value);
+});
--- __Test__/Intern.test.js
+++ __Test__/Intern.test.js
@@ -1,1 +1,20 @@
-const intern = require('../lib/intern');
+const Intern = require('../lib/intern');
+const { it } = require('@jest/globals');
+
+it ('office number should be presented', ()=> {
+  const value = 'UNCC'
+  const e = new Intern('Thurston', 896, 'thurstcx@yahoo.com', value);
+  expect(e.school).toBe(value);
+});
+
+test ('getOfficeNumber() should return officeNumebr', ()=> {
+  const value = 'Intern'
+  const e = new Intern('Thurston', 896, 'thurstcx@yahoo.com', value);
+  expect(e.getSchool).toBe(value);
+});
+
+test ('gotRole() should return \Intern\'', ()=> {
+  const value = 'UNCC'
+  const e = new Intern('Thurston', 896, 'thurstcx@yahoo.com', value);
+  expect(e.getRole).toBe(value);
+});
