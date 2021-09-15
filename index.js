b6edcf4aa6a006faee97d4a58cc1db84022cb634
Thurston Cox <thurstoncox3@gmail.com>
2021-09-14 21:51:36 -0400

    needed to set variables on my index.js pg

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Employee = require('./lib/employee');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager');
 const { ENGINE_METHOD_RAND } = require('constants');
 
 const outputHTML = (html) => {
